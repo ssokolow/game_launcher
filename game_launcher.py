4cd7354c3c7c4a13683f89595d9ad4152eefeea7
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2014-09-04 09:27:47 -0400

    Switch boilerplate to Python3+PyQt5 basics

--- game_launcher.py
+++ game_launcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """An advanced GUI for organizing and launching games on Linux, regardless of
 how they were installed.
@@ -12,41 +12,24 @@ __appname__ = "Unified Game Launcher"
 __version__ = "0.0pre0"
 __license__ = "GNU GPL 3.0 or later"
 
-import logging
-log = logging.getLogger(__name__)
+import sys
+from PyQt5 import QtWidgets
 
 # -- Code Here --
 
 def main():
     """The main entry point, compatible with setuptools entry points."""
-    # pylint: disable=bad-continuation
-    from optparse import OptionParser
-    parser = OptionParser(version="%%prog v%s" % __version__,
-            usage="%prog [options] <argument> ...",
-            description=__doc__.replace('\r\n', '\n').split('\n--snip--\n')[0])
-    parser.add_option('-v', '--verbose', action="count", dest="verbose",
-        default=2, help="Increase the verbosity. Use twice for extra effect")
-    parser.add_option('-q', '--quiet', action="count", dest="quiet",
-        default=0, help="Decrease the verbosity. Use twice for extra effect")
-    # Reminder: %default can be used in help strings.
+    app = QtWidgets.QApplication(sys.argv)
 
-    # Allow pre-formatted descriptions
-    parser.formatter.format_description = lambda description: description
+    w = QtWidgets.QWidget()
+    w.resize(350, 150)
+    w.move(300, 300)
+    w.setWindowTitle(__appname__)
+    w.show()
 
-    opts, args = parser.parse_args()
-
-    # Set up clean logging to stderr
-    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
-                  logging.INFO, logging.DEBUG]
-    opts.verbose = min(opts.verbose - opts.quiet, len(log_levels) - 1)
-    opts.verbose = max(opts.verbose, 0)
-    logging.basicConfig(level=log_levels[opts.verbose],
-                        format='%(levelname)s: %(message)s')
-
-
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
 
 # vim: set sw=4 sts=4 expandtab :
-
