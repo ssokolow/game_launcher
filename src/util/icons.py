54fee0bfa66d18389b0518ffb0c1d4c5b7d7f8b9
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2015-11-20 03:27:19 -0500

    Make pick_icon()'s heuristics smarter
    - Add format precedence
    - Add a simple form of size precedence
    - Use score-based precedence for better nuance
    - Add items.* and terrain.* to NON_ICON_NAMES as a stop-gap

--- src/util/icons.py
+++ src/util/icons.py
@@ -22,8 +22,57 @@ NON_ICON_NAMES = (
     'character*',
     'sheet*',
     'tile*',
+    # TODO: Should really be regex'd with \d* to be less case-specific
+    'items.*',
+    'terrain.*',
 )
 NON_ICON_NAMES_RE = multiglob_compile(NON_ICON_NAMES, re_flags=re.I)
+
+
+
+    """
+
+
+
+
+def calculate_icon_score(filename):
+    # TODO: Prefer square images so we don't wind up using Time Swap's Ouya
+    #       icon by mistake.
+
+    # TODO: Once I've got a regression suite in place, try capturing the
+    #       NEO Scavenger icon by matching for img/*logo.*
+
+    # TODO: I'll need to extract icons from .exe files in Mono-based games
+    #       like Atom Zombie Smasher which don't offer them separately.
+    #       (Also, it should be possible to get additional cues as to a game's
+    #        name by looking for binaries with the same spelling but different
+    #        capitalization when the folder name is all lowercase)
+
+    # TODO: If nothing else matches, look inside things like
+    #       EndlessNuclearKittens.jar to find (16|32|64|128).png
+
+    # TODO: Try to find a way to prefer timeswapIcon.png over icon.svg
+    #        without resorting to rendering the SVG and picking the one
+    #        that's colour rather than sepia-toned grayscale.
+    formats = {
+        '.svg': 5,
+        '.png': 4,
+        '.xpm': 3,
+        '.bmp': 2,
+        '.jpg': 1,
+    }
+
+    base, ext = [x.lower() for x in os.path.splitext(filename)]
+    score = (
+        formats.get(ext, 0) +
+        (2 if 'icon' in base else 0) +
+        (2 if base == 'icon' else 0) +
+        (-1 if filename.startswith('.') else 0)
+    )
+    # Return a sorting key consisting of the score and a cheap approximation
+    # of parsing out things like `128x128` and picking the largest.
+    return (score, int(''.join(s for s in base if s.isdigit()) or 0))
+
 
 def pick_icon(icons, parent_path):
     """Choose the best icon from a set of detected image files.
@@ -43,44 +92,8 @@ def pick_icon(icons, parent_path):
         if not NON_ICON_NAMES_RE.match(img):
             result.append(img)
     icons = result or icons
+    icons.sort(key=calculate_icon_score, reverse=True)
 
-    # Prefer images with icon in the name as icons.
-    result = []
-    for icon in icons:
-        if 'icon' in icon.lower():
-            result.append(icon)
-    icons = result or icons
-
-    # Prefer images named icon.*
-    result = []
-    for icon in icons:
-        significant = os.path.splitext(icon)[0].lower()
-        if significant == 'icon':
-            result.append(icon)
-    icons = result or icons
-
-    # TODO: Prefer square images so we don't wind up using Time Swap's Ouya
-    #       icon by mistake.
-
-    # TODO: Prefer SVG > PNG > XPM > BMP > JPEG
-    #       (But try to find a way to prefer timeswapIcon.png over icon.svg
-    #        without resorting to rendering the SVG and picking the one
-    #        that's colour rather than sepia-toned grayscale)
-
-    # TODO: Once I've got a regression suite in place, try capturing the
-    #       NEO Scavenger icon by matching for img/*logo.*
-
-    # TODO: Need to understand patterns like *_(32|128).png so SuperTuxKart
-    #       reliably gets the bigger icon when it needs to be upscaled.
-
-    # TODO: I'll need to extract icons from .exe files in Mono-based games
-    #       like Atom Zombie Smasher which don't offer them separately.
-    #       (Also, it should be possible to get additional cues as to a game's
-    #        name by looking for binaries with the same spelling but different
-    #        capitalization when the folder name is all lowercase)
-
-    # TODO: If nothing else matches, look inside things like
-    #       EndlessNuclearKittens.jar to find (16|32|64|128).png
 
     # TODO: Make this smarter
     return os.path.join(parent_path, icons[0])
