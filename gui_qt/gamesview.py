a2365b850ca060f23640fabd69b6f9c618b2d4c1
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-09-19 14:28:29 -0400

    testgui_qt: Port the context menu from the GTK+ version
    (In the rawest form possible. Cleanup still to come.)

--- gui_qt/gamesview.py
+++ gui_qt/gamesview.py
@@ -3,8 +3,9 @@
 __author__ = "Stephan Sokolow (deitarion/SSokolow)"
 __license__ = "GNU GPL 3.0 or later"
 
-from PyQt5.QtCore import Qt, pyqtSlot
-from PyQt5.QtWidgets import QListView, QStackedWidget, QTableView
+from PyQt5.QtCore import QProcess, Qt, pyqtSlot
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QListView, QMenu, QStackedWidget, QTableView
 
 class GamesView(QStackedWidget):
     """Encapsulation for the stuff that ties together stack_view_games and its
@@ -35,9 +36,10 @@ class GamesView(QStackedWidget):
 
         #self.listview.setItemDelegate(AugmentedListDelegate(self))
 
-        # Hook up double-click/enter handlers
-        self.listview.activated.connect(self.launchEntry)
-        self.tableview.activated.connect(self.launchEntry)
+        # Hook up double-click/enter/right-click/menukey handlers
+        for view in (self.listview, self.tableview):
+            view.activated.connect(self.launchEntry)
+            view.customContextMenuRequested.connect(self.context_menu_for)
 
     def currentView(self):
         """Retrieve a reference to the currently visible view"""
@@ -61,6 +63,78 @@ class GamesView(QStackedWidget):
         # Synchronize selection behaviour between the two views
         self.selectionmodel = self.tableview.selectionModel()
         self.listview.setSelectionModel(self.selectionmodel)
+
+    # TODO: The popup menu should include:
+    #       - A submenu for selecting which subentry is default (double-click)
+    #       - An option to merge the selected entries which is conditional
+    #         on multiple entries actually being selected.
+    #       - An option to split the selected entry's subentries into entries.
+    #       - An option to change the icon which opens a dialog box with...
+    #           - A preview with a scale slider
+    #           - A "Pick Icon..." button which causes the system to scan the
+    #             game's container (folder, WINEPREFIX, etc.) and display an
+    #             icon picker with all found icons.
+    #             - I'll want to examine gExtractWinIcons to figure out what
+    #               it's doing that I'm not when using wrestool directly.
+    #           - A "Browse Icon..." button which calls up an open dialog.
+    #           - Some kind of cropper to help un-border things like
+    #             GOG's rounded icons.
+    #           - A checkbox to auto-remove a solid-colour background
+    #             like in the icons for Reus, Vessel, Uplink, Escape Goat 2,
+    #             and possibly Beatblasters III and Not The Robots, but not
+    #             Super Meat Boy, Shadowgrounds, Dear Esther, or Antichamber.
+    #           - Some kind of matte adjustment control for upscaling
+    #           - A dropdown to override the choice of scaling algorithm
+    #             on a per-icon basis.
+    #       - A preferences panel which provides...
+    #           - A means of setting launch wrappers like pasuspender
+    #           - A means of setting custom arguments to the game
+    #           - A means of editing subentries?
+    #           - A dropdown to select an antimicro profile to enable on launch
+    #           - Checkboxes to enable or disable LD_PRELOAD hooks
+    #       - ...and what else?
+    # pylint: disable=invalid-name
+    def context_menu_for(self, pos):
+        """Generate and return a context menu for the given position"""
+        view = self.currentView()
+        index = view.indexAt(pos)
+        entry = self.model.data(index, Qt.UserRole)
+
+        menu = QMenu(self)
+
+        # TODO: If there's more than one install prefix detected, group and
+        #  provide section headers.
+        #  (eg. multiple versions of the same game installed in parallel)
+        default_cmd = entry.default_launcher
+        for cmd in sorted(entry.commands,
+                          key=lambda x: (x != default_cmd, x.role, x.name)):
+            # TODO: Move this into the frontend agnostic code
+            # TODO: Use the role name, falling back to Play only if unknown
+            # TODO: Sort by role
+            # TODO: Put a separator between the Play links and the inst/uninst
+            # TODO: Accelerator keys for common roles
+            name = cmd.name if cmd.name != entry.name else 'Play'
+
+            # TODO: Add an "Are you sure?" dialog for install/uninstall
+            action = menu.addAction(name,
+                                    lambda triggered=None, cmd=cmd: cmd.run())
+
+            # TODO: Actually use a customizable default setting
+            if cmd == default_cmd:
+                font = action.font()
+                font.setBold(True)
+                action.setFont(font)
+
+        def open_folder(triggered=None, entry=entry):
+            QProcess(self).start('xdg-open', [entry.base_path])
+
+        menu.addSeparator()
+        menu.addAction("Open Install Folder", open_folder).setEnabled(
+            bool(entry.base_path))
+        menu.addAction("Rename...").setEnabled(False)
+        menu.addAction("Hide").setEnabled(False)
+
+        menu.popup(QCursor.pos())
 
     @pyqtSlot()
     def ensureSelection(self):
