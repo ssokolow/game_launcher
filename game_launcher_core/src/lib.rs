ea383924ebc46f3ff4957aef183c4a281af68502
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2017-04-02 06:00:04 -0400

    Add clippy and rustfmt and document better

--- game_launcher_core/src/lib.rs
+++ game_launcher_core/src/lib.rs
@@ -1,24 +1,28 @@
-#[macro_use] extern crate cpython;
+//! GUI-agnostic core functionality for `game_launcher`.
+//!
+//! **TODO:**
+//!
+//! * Come up with a proper name to replace the `game_launcher` placeholder.
+//! * Also provide language-agnostic C bindings.
+
+#[macro_use]
+extern crate cpython;
 
 use cpython::{PyModule, PyResult};
 
 pub mod util;
 
-// add bindings to the generated python module
-// N.B: names: "librust2py" must be the name of the `.so` or `.pyd` file
-py_module_initializer!(core,
-                       initcore,
-                       PyInit_core, |py, m| {
-    try!(m.add(py, "__doc__", "GUI agnostic core functionality for game_launcher."));
+/// Add binding boilerplate so this can be `import`ed.
+py_module_initializer!(core, initcore, PyInit_core, |py, m| {
+    m.add(py, "__doc__", "GUI-agnostic core functionality for game_launcher.")?;
 
-    let py_util = try!(util::get_python_module(&py));
-    try!(m.add(py, "util", py_util));
+    let py_util = util::get_python_module(&py)?;
+    m.add(py, "util", py_util)?;
     Ok(())
 });
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
--- game_launcher_core/src/util/mod.rs
+++ game_launcher_core/src/util/mod.rs
@@ -1,15 +1,17 @@
-//! TODO: Refactor the Python dependency out
+//! TODO: Refactor the Python dependency out or make it optional
 
 use cpython::{PyModule, PyResult, Python};
 
+/// Centralized definition of all heuristic tuning parameters
 pub mod constants {
-    /// Support binaries which whould be excluded from listings
-    pub const IGNORED_BINARIES: &'static [&'static str] = &[
-        "xdg-*", "flashplayer",
-        "Data.*",
-        "lib*.so.*",
-        "README*",
-    ];
+    /// Globs denoting supporting binaries which should be excluded from listings
+    ///
+    /// TODO: Decide how to incorporate these without messing up detection of
+    ///  things we actually want:
+    ///
+    ///  .mojosetup/*, uninstall-*, java/, node_modules, Shaders, *~, Mono
+    pub const IGNORED_BINARIES: &'static [&'static str] =
+        &["xdg-*", "flashplayer", "Data.*", "lib*.so.*", "README*"];
 
     /// Extensions which denote a likely game installer
     pub const INSTALLER_EXTS: &'static [&'static str] = &[
@@ -21,9 +23,9 @@ pub mod constants {
 
     /// Don't search for metadata inside scripts like "start.sh" if they're bigger
     /// than this size.
-    pub const MAX_SCRIPT_SIZE: usize = 1024 * 1024;  // 1 MiB
+    pub const MAX_SCRIPT_SIZE: usize = 1024 * 1024; // 1 MiB
 
-    /// Files which shouldn't be considered as executables even when marked +x
+    /// Extensions which indicate files shouldn't be considered as executables even when marked +x
     pub const NON_BINARY_EXTS: &'static [&'static str] = &[
         ".dll", ".so", ".dso", ".shlib", ".o", ".dylib",
         ".ini", ".xml", ".txt",
@@ -36,53 +38,58 @@ pub mod constants {
         ".ttf", ".crt",
         ".dl_", ".sc_", ".ex_",
     ];
-    // TODO: .mojosetup/*, uninstall-*, java/, node_modules, xdg-*, Shaders, *~, Mono
 
-    // TODO: Find some way to do a coverage test for this.
-    /// Extensions which denote a likely candidate for the launcher menu
+    /// Extensions which denote likely candidates for the launcher menu
+    ///
+    /// Note: `.com` is intentionally excluded because they're so rare outside
+    ///       of DOSBox and I worry about the potential for false positives
+    ///       caused by it showing up in some game's clever title.
+    ///
+    /// TODO: Find some way to do a coverage test for this.
     pub const PROGRAM_EXTS: &'static [&'static str] = &[
         ".air", ".swf", ".jar",
         ".sh", ".py", ".pl",
         ".exe", ".bat", ".cmd", ".pif",
         ".bin",
         ".desktop",
-        // Note: .com is intentionally excluded because they're so rare outside of
-        //       DOSBox and I worry about the potential for false positives caused
-        //       by it showing up in some game's clever title.
     ];
 
     // TODO: What does the fallback guesser use this for again?
-    pub const RESOURCE_DIRS: &'static [&'static str] = &[
-        "assets",
-        "data", "*_data",
-        "resources",
-        "icons",
-    ];
+    pub const RESOURCE_DIRS: &'static [&'static str] =
+        &["assets", "data", "*_data", "resources", "icons"];
 }
 
+// TODO: I need to solve:
+//
+// * Ensuring a consistent sort order when using this as a sorting key
+// * Using variant names as labels in the GUI
+// * Deciding whether I need to leave room for new variants
 pub mod executables {
+    #[repr(C)]
     pub enum Role {
         Play = -2,
         Configure,
         Unknown,
         Update,
         Install,
-        Uninstall
+        Uninstall,
     }
     // TODO: Implement ToPyObject
 }
 
+/// Called by the top-level `py_module_initializer!` macro to export the symbols in this file
+///
+/// TODO: come up with a macro to remove all of the repeated boilerplate in this.
 pub fn get_python_module(py: &Python) -> PyResult<PyModule> {
     let py = *py;
-    // TODO: come up with a macro to remove all of this republishing boilerplate
-    let py_constants = try!(PyModule::new(py, "constants"));
-    try!(py_constants.add(py, "IGNORED_BINARIES", constants::IGNORED_BINARIES));
-    try!(py_constants.add(py, "INSTALLER_EXTS", constants::INSTALLER_EXTS));
-    try!(py_constants.add(py, "NON_BINARY_EXTS", constants::NON_BINARY_EXTS));
-    try!(py_constants.add(py, "PROGRAM_EXTS", constants::PROGRAM_EXTS));
-    try!(py_constants.add(py, "RESOURCE_DIRS", constants::RESOURCE_DIRS));
+    let py_constants = PyModule::new(py, "constants")?;
+    py_constants.add(py, "IGNORED_BINARIES", constants::IGNORED_BINARIES)?;
+    py_constants.add(py, "INSTALLER_EXTS", constants::INSTALLER_EXTS)?;
+    py_constants.add(py, "NON_BINARY_EXTS", constants::NON_BINARY_EXTS)?;
+    py_constants.add(py, "PROGRAM_EXTS", constants::PROGRAM_EXTS)?;
+    py_constants.add(py, "RESOURCE_DIRS", constants::RESOURCE_DIRS)?;
 
-    let py_util = try!(PyModule::new(py, "util"));
-    try!(py_util.add(py, "constants", py_constants));
+    let py_util = PyModule::new(py, "util")?;
+    py_util.add(py, "constants", py_constants)?;
     Ok(py_util)
 }
